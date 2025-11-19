e9979cc8d220370948092092ebb3626fce521f82
Jérémie abt <abtjeremie@gmail.com>
2025-11-19 18:44:22 +0100

    first commit

---
+++ pubsub.go
@@ -0,0 +1,90 @@
+// Pubsub implements a PubSub library, this is an internal package and for now does not
+// expose online broker, so it can only be used within the same process.
+
+// Rules of business, be aware of them before using this package:
+//   - The order is not guaranteed.
+//   - Some message may be lost if your consumer are too slow, I made this decision because
+//     I'm building this pkg for sse, the example I had in mind was datadog, send new metrics every
+//     x times, so it make sense to just drop data since they are not primordially important.
+//     the correct refresh of the graphs are good but not critical.
+package pubsub
+
+import (
+	"context"
+	"sync"
+)
+
+func NewPubSub(ctx context.Context) *Publisher {
+	p := &Publisher{
+		// channels is a map that store a list of subscribers (chan of bytes) by topic.
+		channels:       make(map[string][]chan []byte),
+		balancer:       make(chan *evt, 1000),
+		subscribeMutex: &sync.Mutex{},
+	}
+
+	// This is the load balancer, it will get all the message and redistribute them so that the Publish
+	// won't block too much.
+	go func() {
+		for {
+			select {
+			// Graceful shutdown
+			case <-ctx.Done():
+				p.close()
+				p.channels = nil
+				p.balancer = nil
+				return
+			case curEvt := <-p.balancer:
+				p.sendMsg(ctx, curEvt.topic, curEvt.msg)
+			}
+		}
+
+	}()
+
+	return p
+}
+
+type evt struct {
+	topic string
+	msg   []byte
+}
+
+type Publisher struct {
+	// channels represent a list of subscribers for a given topic
+	channels       map[string][]chan []byte
+	balancer       chan *evt
+	subscribeMutex *sync.Mutex
+}
+
+func (p *Publisher) sendMsg(ctx context.Context, topic string, msg []byte) {
+	for _, subscriber := range p.channels[topic] {
+		select {
+		case subscriber <- msg:
+			return
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (p *Publisher) close() {
+	for _, subscribers := range p.channels {
+		for _, subscriber := range subscribers {
+			close(subscriber)
+		}
+	}
+
+	close(p.balancer)
+}
+
+func (p *Publisher) Publish(topic string, msg []byte) {
+	p.balancer <- &evt{topic, msg}
+}
+
+func (p *Publisher) Subscribe(topic string) <-chan []byte {
+	p.subscribeMutex.Lock()
+	defer p.subscribeMutex.Unlock()
+
+	newSubscriber := make(chan []byte, 10)
+	p.channels[topic] = append(p.channels[topic], newSubscriber)
+	return newSubscriber
+}
---
+++ pubsub_test.go
@@ -0,0 +1,122 @@
+package pubsub_test
+
+import (
+	"context"
+	"fmt"
+	"ssr/pubsub"
+	"testing"
+	"testing/synctest"
+	"time"
+)
+
+func TestPublisher_Publish(t *testing.T) {
+	t.Parallel()
+
+	publisher := pubsub.NewPubSub(t.Context())
+
+	for i := 0; i < 100; i++ {
+		sub := publisher.Subscribe("test")
+
+		go func() {
+			for {
+				select {
+				case <-t.Context().Done():
+					return
+				case myMsg := <-sub:
+					fmt.Println("received msg : ", string(myMsg))
+				}
+			}
+		}()
+	}
+
+	for i := 0; i < 10000000; i++ {
+		go publisher.Publish("test", []byte("Hello world"))
+	}
+
+	time.Sleep(time.Second * 60 * 5)
+}
+
+func TestSubscriber_Topics(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		publisher := pubsub.NewPubSub(t.Context())
+
+		firstSub := publisher.Subscribe("test")
+		secondSub := publisher.Subscribe("other")
+		firstMessageWanted := []byte("Test message")
+		secondMessageWanted := []byte("Other message")
+
+		var i int
+		go func() {
+			for {
+				select {
+				case <-t.Context().Done():
+					return
+				case msg := <-firstSub:
+					if string(msg) != string(firstMessageWanted) {
+						t.Errorf("received wrong message : %s, wanted : %s", string(msg), string(firstMessageWanted))
+					} else {
+						i++
+					}
+				case msg := <-secondSub:
+					if string(msg) != string(secondMessageWanted) {
+						t.Errorf("received wrong message : %s, wanted : %s", string(msg), string(secondMessageWanted))
+					} else {
+						i++
+					}
+				}
+			}
+		}()
+
+		publisher.Publish("test", firstMessageWanted)
+		publisher.Publish("other", secondMessageWanted)
+
+		synctest.Wait()
+		if i != 2 {
+			t.Errorf("received wrong number of messages : %d, wanted : 2", i)
+		}
+	})
+}
+
+func TestBlockedConsumer(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		testCtx, cancel := context.WithCancel(t.Context())
+
+		publisher := pubsub.NewPubSub(testCtx)
+
+		listOfBlockedSubscriberTopics := []string{"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth", "tenth", "eleventh"}
+
+		listOfBlockedSubscriber := make([]<-chan []byte, 11)
+		for _, topic := range listOfBlockedSubscriberTopics {
+			// Basically we subscribe and never listen on the channel which will cause it to be blocked
+			listOfBlockedSubscriber = append(listOfBlockedSubscriber, publisher.Subscribe(topic))
+		}
+
+		normalConsumerSubscriber := publisher.Subscribe("clean-topic-that-will-be-consumed")
+
+		for i := 0; i < 50; i++ {
+			for _, blockedSubscriber := range listOfBlockedSubscriberTopics {
+				publisher.Publish(blockedSubscriber, []byte(fmt.Sprintf("Message %d", i)))
+			}
+		}
+
+		// Wait all the message has been sent to the slows consumers and are blocking.
+		synctest.Wait()
+
+		publisher.Publish("clean-topic-that-will-be-consumed", []byte("Last message"))
+
+		timeout := time.After(time.Millisecond * 100)
+	loop:
+		for {
+			select {
+			case <-normalConsumerSubscriber:
+				break loop
+			case <-timeout:
+				t.Fatalf("timeout reached without receiving message, " +
+					"it seem that the bottleneck has been sent and the pubsub is in a deadlock")
+			}
+		}
+
+		cancel()
+		synctest.Wait()
+	})
+}
